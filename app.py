df360dfd7432587e82dad7a04209152ff86dfb0b
忘忧北萱草 <wybxc@qq.com>
2022-06-10 15:22:55 +0800

    better ui

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import sys
-import time
 
 import pyperclip
 import streamlit as st
@@ -7,8 +6,18 @@ import streamlit as st
 from generate import generate
 from load_model import get_writer_model, gpus, list_models
 
-if 'gpu_checked' in st.session_state:
-    st.session_state['gpu_checked'] = True
+st.set_page_config(
+    page_title="Misaka Writer",
+    layout="centered",
+    menu_items={
+        "Get Help": "https://github.com/Wybxc/misaka-writer-ui",
+        "Report a bug": "https://github.com/pass-lin/misaka-writer/issues",
+        "About": "# Misaka Writer \n基于encoder-decoder结构的续写小说模型。",        
+    },
+)
+
+if "gpu_checked" in st.session_state:
+    st.session_state["gpu_checked"] = True
 elif not gpus:
     sys.stderr.write("No available GPU found.\n")
 else:
@@ -25,19 +34,29 @@ if "outputs" not in st.session_state:
 if "time_consumed" not in st.session_state:
     st.session_state["time_consumed"] = 0
 
-model_path = st.sidebar.selectbox("选择模型：", list(list_models()))
+# 基础设置
+
+model_path = st.sidebar.selectbox(
+    "选择模型：", list(list_models()), help="模型路径在 misaka_writer/models/ 下"
+)
 support_english = st.sidebar.checkbox(
-    "英文模式", value=st.session_state["current_support_english"]
+    "英文模式",
+    value=st.session_state["current_support_english"],
+    help="续写英语时需要打开，其他情况下关闭。仅限综合模型使用。",
 )
 model = st.session_state["current_model"]
 
 st.sidebar.markdown("---")
 
+# 高级设置
+
 max_len = int(
     st.sidebar.number_input("续写最大长度：", min_value=50, max_value=512, value=512)
 )
 nums = int(st.sidebar.number_input("生成下文的数量：", min_value=1, max_value=10, value=3))
-mode = st.sidebar.selectbox("续写模式：", ("topp", "topk"))
+mode = st.sidebar.selectbox("续写模式：", ("topp", "topk"), help="topk 速度更快，但结果质量可能略差于 topp")
+
+# 主要部分
 
 text = st.text_area(
     "输入开头（建议50~250字）：",
@@ -48,7 +67,16 @@ text = st.text_area(
 “鲁智深，我与你不共戴天！”
 """.strip(),
     height=250,
+    help="输入过长会显著降低生成结果质量",
 )
+left, right = st.columns(2)
+start_generate = left.button("开始生成")
+right.caption(
+    f'<div style="text-align: right">当前字数: {len(text)}</div>', unsafe_allow_html=True
+)
+
+
+# 主题逻辑
 
 if model_path and (
     model_path != st.session_state["current_model_path"]
@@ -84,10 +112,15 @@ class ProgressBar:
         self.finish()
 
 
-if model and st.button("开始生成"):
+if model and start_generate:
     with ProgressBar(total=max_len * (len(text) // 400 + 1) + 10) as pbar:
         outputs, time_consumed = generate(
-            model, text, max_len=max_len, nums=nums, mode=mode, step_callback=pbar.update
+            model,
+            text,
+            max_len=max_len,
+            nums=nums,
+            mode=mode,
+            step_callback=pbar.update,
         )
         st.session_state["outputs"] = outputs
         st.session_state["time_consumed"] = time_consumed
@@ -99,4 +132,8 @@ if st.session_state["outputs"]:
     for i, output in enumerate(outputs):
         st.subheader(f"续写{i + 1}")
         st.write(output.replace("\n", "\n\n"))
-        st.button("复制", key=i, on_click=(lambda output: lambda: pyperclip.copy(output))(output))
+        st.button(
+            "复制",
+            key=i,
+            on_click=(lambda output: lambda: pyperclip.copy(output))(output),
+        )
